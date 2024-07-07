b1f2e12d57b4cb5f11b35ec68472897b751ac689
Kate <ekaterina21051988@yandex.by>
2024-07-07 18:33:56 +0300

    Search component has been created

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, Component } from 'react';
 import { Character } from '../models';
 import logo from '../assets/rick-and-morty.png';
+import ErrorButton from './ErrorButton';
 
 interface SearchProps {
   renderResults: (results: Character[]) => void;
@@ -28,7 +29,7 @@ class Search extends Component<SearchProps, SearchState> {
     this.props.setLoading(true);
     const trimmedSearchItem = this.state.searchCard.trim();
     let apiUrl = `https://rickandmortyapi.com/api/character/`;
-    
+
     if (trimmedSearchItem) {
       apiUrl += `?name=${trimmedSearchItem}`;
     }
@@ -40,7 +41,7 @@ class Search extends Component<SearchProps, SearchState> {
     } catch (error) {
       console.error('Error fetching data:', error);
     }
-    
+
     this.props.setLoading(false);
   };
 
@@ -51,13 +52,23 @@ class Search extends Component<SearchProps, SearchState> {
   render() {
     return (
       <div className="search-bar">
-        <img src={logo} alt="rick-and-morty logo" width={150}/>
-        <input
-          type="text"
-          placeholder='Search character...'
-          value={this.state.searchCard}
-          onChange={this.handleInputChange}    />
-        <button onClick={this.fetchData} disabled={this.state.isLoading}>Search</button>
+        <img src={logo} alt="rick-and-morty logo" width={150} />
+        <ErrorButton />
+        <div>
+          <input
+            type="text"
+            placeholder="Search character..."
+            value={this.state.searchCard}
+            onChange={this.handleInputChange}
+          />
+          <button
+            className="search-bar-button"
+            onClick={this.fetchData}
+            disabled={this.state.isLoading}
+          >
+            Search
+          </button>
+        </div>
       </div>
     );
   }
