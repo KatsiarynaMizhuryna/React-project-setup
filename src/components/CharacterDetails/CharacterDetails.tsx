305e40ad9b6014fccc3b5e4d09511b511c43d67b
Kate <ekaterina21051988@yandex.by>
2024-07-14 17:36:05 +0300

    Logic for app has been modified

--- src/components/CharacterDetails/CharacterDetails.tsx
+++ src/components/CharacterDetails/CharacterDetails.tsx
@@ -1,38 +1,38 @@
-import { Character } from "../../models";
+import { Character } from '../../models';
 
 interface CharacterDetailsProps {
-    character: Character;
-  }
-  
-  const CharacterDetails: React.FC<CharacterDetailsProps> = ({ character }) => {
-    return (
-      <div className="character-card">
-        <img
-          src={character.image}
-          alt={character.name}
-          className="character-image"
-        />
-        <div className="character-info">
-          <h2>
-            <strong>Name: </strong>
-            {character.name}
-          </h2>
-          <p>
-            <strong>Status: </strong> {character.status}
-          </p>
-          <p>
-            <strong>Species:</strong> {character.species}
-          </p>
-          <p>
-            <strong>Gender:</strong> {character.gender}
-          </p>
-          <p>
-            <strong>Last Known Location:</strong>{' '}
-            <a href={character.location.url}>{character.location.name}</a>
-          </p>
-        </div>
+  character: Character;
+}
+
+const CharacterDetails: React.FC<CharacterDetailsProps> = ({ character }) => {
+  return (
+    <div className="character-card">
+      <img
+        src={character.image}
+        alt={character.name}
+        className="character-image"
+      />
+      <div className="character-info">
+        <h2>
+          <strong>Name: </strong>
+          {character.name}
+        </h2>
+        <p>
+          <strong>Status: </strong> {character.status}
+        </p>
+        <p>
+          <strong>Species:</strong> {character.species}
+        </p>
+        <p>
+          <strong>Gender:</strong> {character.gender}
+        </p>
+        <p>
+          <strong>Last Known Location:</strong>{' '}
+          <a href={character.location.url}>{character.location.name}</a>
+        </p>
       </div>
-    );
-  };
-  
-  export default CharacterDetails;
+    </div>
+  );
+};
+
+export default CharacterDetails;
