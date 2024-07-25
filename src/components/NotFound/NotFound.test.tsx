4f3a131389752686ac05d6a06acb713888d599ec
Kate <ekaterina21051988@yandex.by>
2024-07-25 11:28:37 +0300

    feat: tests for NotFound component have been added

---
+++ src/components/NotFound/NotFound.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import NotFound from './NotFound';
+
+jest.mock('../../assets/not-found.png', () => 'not-found-image-url');
+
+describe('NotFound component', () => {
+  test('renders the component correctly', () => {
+    render(<NotFound />);
+
+    expect(
+      screen.getByText('Nothing found... Try it once more!')
+    ).toBeInTheDocument();
+    const image = screen.getByAltText('not-found');
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'not-found-image-url');
+  });
+
+  test('has the correct styles', () => {
+    render(<NotFound />);
+
+    const container = screen.getByText(
+      'Nothing found... Try it once more!'
+    ).parentElement;
+    expect(container).toHaveClass('not_found');
+
+    const image = screen.getByAltText('not-found');
+    expect(image).toHaveClass('not_found_image');
+  });
+});
